a5daf21b6deca85b04aefa42e44bc699789f6af1
Samuel Lubelsky <slubelsky30@stuy.edu>
2022-12-07 08:52:16 -0500

    verify session added

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,19 @@ from flask import Flask             #facilitate flask webserving
 from flask import render_template, request   #facilitate jinja templating
 from flask import session, redirect, url_for, make_response 
 import os
+import requests
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 @app.route("/")
 def home():
-    
+    if verify_session():   
+        pass 
+
+def verify_session():
+    return True
+    #if username & pwd in cookies
+    if 'username' in session and 'password' in session:
+        if db_tools.verify_account(session['username'], session['password']):
+            return True
+    return False
