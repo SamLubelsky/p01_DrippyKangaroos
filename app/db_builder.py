de63dbb266121468033dd625325cc2d0b079b691
Anson <awong30@stuy.edu>
2022-12-10 04:19:52 -0500

    part storing news article

--- app/db_builder.py
+++ app/db_builder.py
@@ -1,8 +1,13 @@
 import sqlite3
-
+import newsapi
+from datetime import date
 
 users = ("(username TEXT, password TEXT)")
-article = ("(articleId INTEGER PRIMARY KEY, url TEXT, summary TEXT)")
+home_article = ("(articleId INTEGER PRIMARY KEY, date TEXT)")
+article_info = ("(articleId INTEGER PRIMARY KEY, title TEXT, date_published TEXT, author TEXT, summary TEXT, url TEXT)")
+past_queries = ("(query INTEGER PRIMARY KEY AUTOINCREMENT, query_text TEXT, date TEXT, articles INT)")
+article_tags = ("(articleId INTEGER PRIMARY KEY, topic TEXT)")
+
 def data_query(table, info = None):
     db = sqlite3.connect("database.db")
     c = db.cursor()
@@ -13,10 +18,14 @@ def data_query(table, info = None):
     db.commit()
     db.close()
     return output
+    
 def create_table(name, outline):
     data_query(f"CREATE TABLE IF NOT EXISTS {name} {outline}")
 
-create_table("Article", article)
+create_table("Home", home_article)
+create_table("News", article_info)
+create_table("Tags", article_tags)
+create_table("Query", past_queries)
 create_table("User", users)
 
 def get_table_list(name):
@@ -27,6 +36,7 @@ def get_table_list(name):
     db.commit()
     db.close()
     return output
+    
 
 def add_account(username, password):
     if not(used(username,"User")):
@@ -48,3 +58,46 @@ def used(name,table):
         if i[0] == name:
             return True
     return False
+
+def check_date(home):
+    if home == True:
+        home_articles = get_table_list("Home")
+        for article in home_articles:
+            if home_articles[1] == date.today():
+                return True
+    else: 
+        queries = get_table_list("Query")
+        for query in queries:
+            if query[2] == date.today():
+                return True
+    return False
+        
+        
+#def home():
+    #if not (check_date(True)):
+        #db = sqlite3.connect("database.db")
+        #c = db.cursor()
+        ## TO BE ADDED TO NEWSAPI MAYBE
+            #def url(article):
+            #    return article["url"]
+            #def title(article):
+            #    return article["title"]
+            #def desc(article):
+            #    return article["description"]
+            #def date_published(article):
+            #    return article["publishedAt"]{:10}
+        ## END
+        ## DOESN'T INCLUDE THE TOPICS (EX.BUSINESS) -> ALSO NO TAGS
+            #for article in newsapi.headlines():
+                #url = newsapi.url(article)
+                #title = newsapi.title(article)
+                #desc = newsapi.disc(article)
+                #date = newsapi.date(article)
+                #author = newsapi.author(article)
+                #id = c.fetchone();
+                #id = int(id[0])
+                #c.execute("INSERT INTO News VALUES (?, ?, ?, ?, ?, ?)", (id, title, date, author, desc, url))
+                #c.execute("INSERT INTO Home VALUES (?, ?)", (id, date.today()))
+            #db.commit()
+            #db.close()
+    #return get_table_list("Home")
