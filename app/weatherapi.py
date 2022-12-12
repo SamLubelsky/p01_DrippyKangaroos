00d83d0ff8ba80e85721a384d35941177e7b4a7c
Samuel Lubelsky <slubelsky30@stuy.edu>
2022-12-12 10:04:12 -0500

    added weather getting function

--- app/weatherapi.py
+++ app/weatherapi.py
@@ -2,15 +2,28 @@ import json
 import requests
 with open('keys/key_openweather.txt', 'r') as f:
     key = f.read().strip()
-    key = "361f1468f4f95c0aff4c25ae9e3c7b3d"
 print(key)
 nylat = "40.717831142775566" 
 nylon = "-74.0137791697529"
 endpoint = "https://api.openweathermap.org/data/2.5/weather"
-response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={nylat}&lon={nylon}&appid={key}")
+# response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={nylat}&lon={nylon}&appid={key}")
+#print(json.loads(response.text))
+def kelvin_to_fahrenheit(temp):
+    F = 1.8*(temp-273) + 32
+    return F
+def getweatherdata(lat=nylat, lon=nylon):
+    response = requests.get(f"{endpoint}?lat={nylat}&lon={nylon}&appid={key}")
+    weather_info = json.loads(response.text)
+    icon = weather_info['weather'][0]['icon']
+    iconUrl = f"openweathermap.org/img/wn/{icon}@2x.png"
+    temp_min = kelvin_to_fahrenheit(weather_info["main"]["temp_min"])
+    temp_max = kelvin_to_fahrenheit(weather_info["main"]["temp_max"])
+    return {'low': temp_min,
+            'high': temp_max,
+            'img_url':iconUrl}
+#print(getweatherdata())
 # response = requests.get(endpoint,
 # {"appid": key,
 #  "lat": nylat,
 #  "lon": nylon,
 #  "unis": "imperial"})
-print(response.text)
