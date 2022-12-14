56eeabc6308c5394cbd100a85d0d9540c430b4b9
Daniel Yentin <dyentin30@stuy.edu>
2022-12-14 09:27:24 -0500

    finished stock requests with stock api

--- app/newsapi.py
+++ app/newsapi.py
@@ -1,17 +1,17 @@
 from urllib.request import urlopen
 import json
 
-startpoint = "https://newsapi.org/v2/"
+startpoint = "https://newsapi.org/v2"
 api_key = ""
 with open("keys/key_newsapi.txt", 'r') as k:
     api_key = k.read().strip() 
 
 def request_articles(query, n=1):
-    url = f"{startpoint}everything?q={query}&language=en&apiKey={api_key}"
+    url = f"{startpoint}/everything?q={query}&language=en&apiKey={api_key}"
     return request(url, n)
 
 def request_top_headlines(category, n=10):
-    url = f"{startpoint}top-headlines?category={category}&language=en&apiKey={api_key}"
+    url = f"{startpoint}/top-headlines?category={category}&language=en&apiKey={api_key}"
     return request(url, n)
 
 def request(url, n):
@@ -21,13 +21,13 @@ def request(url, n):
 
 def article_info(article):
     return {
-            "url": article["url"],
-            "image": article["urlToImage"],
-            "title": article["title"], 
-            "description": article["description"],
-            "date": article["publishedAt"][:10],
-            "author": article["author"]
-        }
+        "url": article["url"],
+        "image": article["urlToImage"],
+        "title": article["title"], 
+        "description": article["description"],
+        "date": article["publishedAt"][:10],
+        "author": article["author"]
+    }
 
 
 if __name__ == "__main__":
--- app/stockapi.py
+++ app/stockapi.py
@@ -1,7 +1,19 @@
-from urllib.request import urlopen
-import json
+import requests
 
-startpoint = "https://paper-api.alpaca.markets/"
-api_key = ""
+startpoint = "https://data.alpaca.markets/v2/stocks"
+API_KEY_ID = ""
+SECRET_KEY = ""
 with open("keys/key_newsapi.txt", 'r') as k:
-    api_key = k.read().strip() 
+    k = k.read().strip().splitlines()
+    API_KEY_ID = k[0]
+    SECRET_KEY = k[1]
+
+headers = {
+    "APCA-API-KEY-ID": API_KEY_ID,
+    "APCA-API-SECRET-KEY": SECRET_KEY,
+}
+
+def request_stock(stock):
+    url = f"{startpoint}/{symbol}/quotes/latest"
+    response = requests.get(url, headers=headers)
+    return response.json()
