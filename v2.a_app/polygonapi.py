df0cbe88d7a3c99adb87c7670f317155cf6b742c
Anson <awong30@stuy.edu>
2022-12-16 05:49:46 -0500

    part picking stocks (t20 gains n losses)

--- v2.a_app/polygonapi.py
+++ v2.a_app/polygonapi.py
@@ -17,8 +17,21 @@ yestdate = todate - timedelta(days = 1)
 c = RESTClient(key)
 
 def request_stock_agg(stock):
-    agg = cast(HTTPResponse,c.get_aggs(stock, 1, "day", yestdate, todate, raw=True,),)
+    agg_url = cast(HTTPResponse,c.get_aggs(stock, 1, "day", yestdate, todate, raw=True,),)
     #url = f"https://api.polygon.io/v2/aggs/ticker/{stock}/range/1/day/{yestdate}/{todate}"
-    return agg.data
+    return agg_url.data
 
 print(request_stock_agg("AAPL"))
+
+def stocks():
+    #top = cast(HTTPResponse,c.get_snapshot_all("stocks"),)
+    #top 20s
+    gainers = cast(HTTPResponse,c.get_snapshot_all("stocks", "gainers",),)
+    losers = cast(HTTPResponse,c.get_snapshot_all("stocks", "losers",),)
+    #s.data[tickers][ticker]
+    #https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers?apiKey=*
+
+stocks()
+#Resources/Docs Used (for further reference)
+#https://github.com/polygon-io/client-python
+#https://polygon.io/docs/stocks/getting-started
